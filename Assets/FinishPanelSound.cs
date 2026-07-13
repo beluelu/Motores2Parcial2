43f7327f224651790db1bf2cf81eb4fcf15cb9d1
Belu <beludurannn@gmail.com>
2026-07-13 04:10:25 -0300

    Update Level1

--- Assets/FinishPanelSound.cs
+++ Assets/FinishPanelSound.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 
 [RequireComponent(typeof(AudioSource))]
@@ -6,13 +7,30 @@ public class FinishPanelSound : MonoBehaviour
 {
     private AudioSource audioSource;
 
-    void Awake()
+    private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
+        if (MusicFade.Instance != null)
+        {
+            MusicFade.Instance.LowerVolume();
+        }
+
         audioSource.Play();
+
+        StartCoroutine(RestoreMusic());
+    }
+
+    IEnumerator RestoreMusic()
+    {
+        yield return new WaitForSecondsRealtime(audioSource.clip.length);
+
+        if (MusicFade.Instance != null)
+        {
+            MusicFade.Instance.RestoreVolume();
+        }
     }
 }
