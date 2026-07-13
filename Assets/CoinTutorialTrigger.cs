1110f0a6785ab93218896a66c931c172282d3fad
Lucks2004 <lucase.fonseca@davinci.edu.ar>
2026-07-13 05:02:45 -0300

    Update CoinTutorial

--- Assets/CoinTutorialTrigger.cs
+++ Assets/CoinTutorialTrigger.cs
@@ -6,18 +6,24 @@ public class CoinTutorialTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
-
         if (!other.CompareTag("Player"))
             return;
 
-        
+        if (topMessage == null)
+        {
+            topMessage = FindFirstObjectByType<TopMessage>();
+        }
+
+        if (topMessage == null) return;
 
         topMessage.ShowMessage("¡Collect all the coins!");
 
         TutorialManager tutorial = FindFirstObjectByType<TutorialManager>();
 
-        tutorial.waitingForTutorial = false;
+        if (tutorial != null)
+        {
+            tutorial.waitingForTutorial = false;
+        }
 
         Destroy(gameObject);
     }
