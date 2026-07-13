d1505619a547aa2f38061a3011524a0f761f9f52
Belu <beludurannn@gmail.com>
2026-07-12 21:26:04 -0300

    Update Level1 Sprites and buttons

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -1,20 +1,42 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using System.Collections;
 
 public class LoadingScreen : MonoBehaviour
 {
     public Slider loadingSlider;
+    public TMP_Text loadingPercent;
+
+    public float loadingTime = 5f; 
 
     IEnumerator Start()
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(LoadingManager.sceneToLoad);
 
-        while (!operation.isDone)
+        
+        operation.allowSceneActivation = false;
+
+        float timer = 0f;
+
+        while (timer < loadingTime)
         {
-            loadingSlider.value = operation.progress / 0.9f;
+            timer += Time.deltaTime;
+
+            float progress = Mathf.Clamp01(timer / loadingTime);
+
+            loadingSlider.value = progress;
+            loadingPercent.text = Mathf.RoundToInt(progress * 100f) + "%";
+
             yield return null;
         }
+
+       
+        loadingSlider.value = 1f;
+        loadingPercent.text = "100%";
+
+        
+        operation.allowSceneActivation = true;
     }
 }
